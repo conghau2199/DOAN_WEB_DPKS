e7a879f06192e90c6f5aa353f46d4e671319872b
sinhvien <sinhvien>
2020-06-17 13:43:46 +0700

    C1

---
+++ CaChepFinal2/Data/DataModel/ChuongTrinh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CaChepFinal2.Data.DataModel
+{
+    public interface IChuongTrinh
+    {
+        IQueryable<ChuongTrinh> GetAll();
+        ChuongTrinh GetOneById(int? id);
+        void New(ChuongTrinh obj);
+        void Edit(ChuongTrinh obj);
+        void Delete(int id);
+    }
+    public  class ChuongTrinh
+    {
+        public ChuongTrinh()
+        {
+            Phongs = new HashSet<Phong>();
+        }
+
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string TenChuongTrinh { get; set; }
+        [Display(Name = "Increate or Decreate Price")]
+        public int TiLeThayDoiGia { get; set; }
+
+        [Display(Name = "isInCreaet ")]
+
+        public bool IsTang { get; set; }
+
+        public virtual ICollection<Phong> Phongs { get; set; }
+    }
+}
---
+++ CaChepFinal2/Data/DataModel/DatPhong.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace CaChepFinal2.Data.DataModel
+{
+    public interface  IDatPhong
+    {
+        IQueryable<DatPhong> GetAll();
+        DatPhong GetOneById(int? id);
+        int New(DatPhong DatPhong);
+        void Edit(DatPhong DatPhong);
+        void Delete(int id);
+    }
+
+    public  class DatPhong
+    {
+        public DatPhong()
+        {
+            ChiTietDatPhong = new HashSet<ChiTietDatPhong>();
+            ChiTietDichVuDatPhong = new HashSet<ChiTietDichVuDatPhong>();
+        }
+
+        public int Id { get; set; }
+        [Display(Name = "Full Name")]
+        public string TenNguoiDat { get; set; }
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Display(Name = "CMND")]
+        public string Cmnd { get; set; }
+        [Display(Name = "PhoneNumber")]
+        public string Sdt { get; set; }
+
+        [Column(TypeName = "Money")]
+        [Display(Name = "Money Reservation")]
+        public decimal TienDatCoc { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Time Check in")]
+        public DateTime ThoiGianNhanPhongDuKien { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Time Check out")]
+        public DateTime ThoiGianTraPhongDuKien { get; set; }
+        [Column(TypeName = "Money")]
+        [Display(Name = "Sum Money ")]
+        public decimal TongTien { get; set; }
+
+        public int? AccoutId { get; set; }
+        public virtual AccountSys Account { get; set; }
+
+        public virtual ICollection<ChiTietDatPhong> ChiTietDatPhong { get; set; }
+        public virtual ICollection<ChiTietDichVuDatPhong> ChiTietDichVuDatPhong { get; set; }
+    }
+}
